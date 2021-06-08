ece25f7ad8eb7dccc8ce5bb4885ef7e32941c897
Sidibé Cheick Ahmed <chs.ahmed_pro02@outlook.com>
2021-06-08 15:07:19 +0200

    add form package and fix base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,29 @@
-module.exports = {
-	purge: [
-		'./components/**/*.{vue,js}',
-		'./layouts/**/*.vue',
-		'./pages/**/*.vue',
-	],
-	mode: 'jit',
-	darkMode: 'class',
-	theme: {
-		extend: {
-			colors: {
-				'base-light-gray': '#191a1b',
-				'base-red-darken-3' : '#c62828',
-				'base-yellow': '#fbf3ab'
-			}
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
-}
+  const colors = require('tailwindcss/colors')
+
+  module.exports = {
+  	purge: [
+  	'./components/**/*.{vue,js}',
+  	'./layouts/**/*.vue',
+  	'./pages/**/*.vue',
+  	],
+  	mode: 'jit',
+  	darkMode: 'class',
+  	theme: {
+  		extend: {
+  			colors: {
+  				'base-light-gray': '#051623',
+  				'base-red-darken-3' : '#b93c4e',
+  				'base-yellow': '#fbf3ab',
+  				'base-yellow-2': '#e29337',
+  				'warm-gray': colors.warmGray,
+  				teal: colors.teal
+  			}
+  		},
+  	},
+  	variants: {
+  		extend: {},
+  	},
+  	plugins: [
+  		require('@tailwindcss/forms'),
+  	],
+  }
