8f0fe95ff02c431a9e70fcc640945a260b6030f5
Sidibé Cheick Ahmed <chs.ahmed_pro02@outlook.com>
2021-06-08 17:43:31 +0200

    update navigation

--- components/commons/Navigation.vue
+++ components/commons/Navigation.vue
@@ -76,7 +76,7 @@
 					<nuxt-link :to="{ name : 'tournaments' }" class="text-base font-medium text-gray-500 hover:text-gray-900">
 						Tournois
 					</nuxt-link>
-					<nuxt-link :to="{ name : '' }" class="text-base font-medium text-gray-500 hover:text-gray-900">
+					<nuxt-link :to="{ name : 'partners' }" class="text-base font-medium text-gray-500 hover:text-gray-900">
 						Partenaires
 					</nuxt-link>
 					<nuxt-link :to="{ name : '' }" class="text-base font-medium text-gray-500 hover:text-gray-900">
@@ -184,7 +184,7 @@
 
 									<nuxt-link :to="{ name : 'tournaments' }" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Tournois</nuxt-link>
 
-									<nuxt-link :to="{ name : '' }" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Partenaires</nuxt-link>
+									<nuxt-link :to="{ name : 'partners' }" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Partenaires</nuxt-link>
 
 									<nuxt-link :to="{ name : '' }" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Donations</nuxt-link>
 								</nav>
