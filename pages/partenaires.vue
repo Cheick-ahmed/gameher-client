b1983715cbba7761a975591b127e3cbf9f19b745
Clément Sabourin <clement.sabourin@hetic.net>
2021-06-08 12:20:26 +0200

    beginning

---
+++ pages/partenaires.vue
@@ -0,0 +1,125 @@
+<template>
+  <div>
+    	<div class="max-w-7xl mx-auto text-5xl py-16 ">
+		<div class="bg-warm-gray-50">
+			<div>
+				<base-header-section description-color="text-base-red-darken-3">
+					<template #title>
+						Nos partenaires
+					</template>
+					<template #description>
+            Ils nous soutiennent et nous les en remerçions
+					</template>
+				</base-header-section>
+			</div>
+		</div>
+	</div>
+
+  		<!-- Alternating Feature Sections -->
+		<div class="relative pt-16 pb-32 overflow-hidden">
+			<div aria-hidden="true" class="absolute inset-x-0 top-0 h-48 from-gray-100"></div>
+			<div class="relative">
+				<div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+					<div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+						<div>
+							<div class="mt-6">
+								<h2 class="text-4xl text-base-red-darken-3 font-extrabold tracking-tight text-gray-900">
+									LMN8
+								</h2>
+								<p class="mt-4 text-lg text-gray-500">
+									Eliminate.fr a été crée dans le but de répondre à la demande de personnalisation textile Esport, fabrication française
+								</p>
+							</div>
+						</div>
+						
+					</div>
+					<div class="mt-12 sm:mt-16 lg:mt-0">
+						<div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+              <img class="mt-24 w-64" src="https://gameher.fr/uploads/partners/lmn8_noir-5e9189665724e.png" alt="lmn8">
+            </div>
+					</div>
+				</div>
+			</div>
+			<div class="mt-24">
+				<div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+					<div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+						<div>
+							<div class="mt-6">
+								<h2 class="text-4xl text-base-red-darken-3 font-extrabold tracking-tight text-gray-900">
+									NEW GO
+								</h2>
+								<p class="mt-4 text-lg text-gray-500">
+									Des voyages geeks uniques. Des thématiques exceptionnelles pour tous les amoureux de cinéma, de mangas, de séries et de science-fiction
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+						<div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+              <img class="mt-40 w-64" src="https://gameher.fr/uploads/partners/lmn8_noir-5e9189665724e.png" alt="lmn8">
+            </div>
+					</div>
+				</div>
+			</div>
+		</div>
+    	<!-- Alternating Feature Sections -->
+		<div class="relative pt-16 pb-32 overflow-hidden">
+			<div aria-hidden="true" class="absolute inset-x-0 top-0 h-48 bg-gradient-to-b from-gray-100"></div>
+			<div class="relative">
+				<div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+					<div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+						<div>
+							<div class="mt-6">
+								<h2 class="text-4xl text-base-red-darken-3 font-extrabold tracking-tight text-gray-900">
+									ESPORT AGORA
+								</h2>
+								<p class="mt-4 text-lg text-gray-500">
+									Esport Agora est un organisme visant à développer la scène esportive : Simplifier les actions des plus petites équipes, leur faciliter l’accès aux meilleurs services professionnels, tout en travaillant à la structuration de l’esport.
+								</p>
+							</div>
+						</div>
+						
+					</div>
+					<div class="mt-12 sm:mt-16 lg:mt-0">
+						<div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+              <img class="mt-24 w-64" src="https://gameher.fr/uploads/partners/lmn8_noir-5e9189665724e.png" alt="lmn8">
+            </div>
+					</div>
+				</div>
+			</div>
+			<div class="mt-24">
+				<div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+					<div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+						<div>
+							<div class="mt-6">
+								<h2 class="text-4xl text-base-red-darken-3 font-extrabold tracking-tight text-gray-900">
+									KONIX INTERACTIVE
+								</h2>
+								<p class="mt-4 text-lg text-gray-500">
+									Notre savoir-faire s’appuie sur notre capacité à écouter nos clients pour leur proposer des accessoires performants, accessibles et conviviaux et à anticiper les besoins naissants qu’induisent les évolutions technologiques de demain.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+						<div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+              <img class="mt-40 w-64" src="~/assets/img/logo_konix-black.png" alt="lmn8">
+            </div>
+					</div>
+				</div>
+			</div>
+		</div>
+  </div>
+</template>
+
+
+<script>
+export default {
+	data () {
+		return {
+
+		}
+	}
+}
+</script>
+</template>
