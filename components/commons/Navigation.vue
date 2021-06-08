a5e6a367f3c6120c38a17a6442baf927a65ff523
Sidibé Cheick Ahmed <chs.ahmed_pro02@outlook.com>
2021-06-08 15:07:43 +0200

    update socials media svg icons

--- components/commons/Navigation.vue
+++ components/commons/Navigation.vue
@@ -3,10 +3,10 @@
 		<div class="relative bg-white">
 			<div class="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-8">
 				<div class="flex justify-start lg:w-0 lg:flex-1">
-					<a href="#">
-						<span class="sr-only">Workflow</span>
-						<img class="h-8 w-auto sm:h-16 transform -translate-x-2" src="~/assets/SVG/GH_black_horizontal.svg" alt="">
-					</a>
+					<nuxt-link :to="{ name : 'index' }">
+						<span class="sr-only">Game'Her</span>
+						<img class="h-8 w-auto sm:h-20 transform -translate-x-2" src="~/assets/SVG/GH_color_horizontal.svg" alt="">
+					</nuxt-link>
 				</div>
 				<div class="-mr-2 -my-2 md:hidden">
 					<button type="button" @click.prevent="mobileMenu = true" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
@@ -39,26 +39,26 @@
 											</h3>
 											<ul class="mt-4 space-y-4">
 												<li class="text-base truncate">
-													<a href="#" class="font-medium text-gray-900 hover:text-gray-700">
+													<nuxt-link :to="{ name : '' }" class="font-medium text-gray-900 hover:text-gray-700">
 														Boost your conversion rate
-													</a>
+													</nuxt-link>
 												</li>
 
 												<li class="text-base truncate">
-													<a href="#" class="font-medium text-gray-900 hover:text-gray-700">
+													<nuxt-link :to="{ name : '' }" class="font-medium text-gray-900 hover:text-gray-700">
 														How to use search engine optimization to drive traffic to your site
-													</a>
+													</nuxt-link>
 												</li>
 
 												<li class="text-base truncate">
-													<a href="#" class="font-medium text-gray-900 hover:text-gray-700">
+													<nuxt-link :to="{ name : '' }" class="font-medium text-gray-900 hover:text-gray-700">
 														Improve your customer experience
-													</a>
+													</nuxt-link>
 												</li>
 											</ul>
 										</div>
 										<div class="mt-5 text-sm">
-											<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> View all posts <span aria-hidden="true">&rarr;</span></a>
+											<nuxt-link :to="{ name : '' }" class="font-medium text-indigo-600 hover:text-indigo-500"> View all posts <span aria-hidden="true">&rarr;</span></nuxt-link>
 										</div>
 									</div>
 								</div>
@@ -66,22 +66,22 @@
 						</transition>
 					</div>
 
-					<a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+					<nuxt-link :to="{ name : '' }" class="text-base font-medium text-gray-500 hover:text-gray-900">
 						Esport
-					</a>
+					</nuxt-link>
 
-					<a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+					<nuxt-link :to="{ name : '' }" class="text-base font-medium text-gray-500 hover:text-gray-900">
 						WebTV
-					</a>
-					<a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+					</nuxt-link>
+					<nuxt-link :to="{ name : '' }" class="text-base font-medium text-gray-500 hover:text-gray-900">
 						Tournois
-					</a>
-					<a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+					</nuxt-link>
+					<nuxt-link :to="{ name : '' }" class="text-base font-medium text-gray-500 hover:text-gray-900">
 						Partenaires
-					</a>
-					<a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+					</nuxt-link>
+					<nuxt-link :to="{ name : '' }" class="text-base font-medium text-gray-500 hover:text-gray-900">
 						Donation
-					</a>
+					</nuxt-link>
 
 					<div class="relative" v-click-outside="hideTeamFlyoutMenu">
 						<!-- Item active: "text-gray-900", Item inactive: "text-gray-500" -->
@@ -98,7 +98,7 @@
 							<div v-if="teamFlyoutMenu" class="absolute left-1/2 z-10 transform -translate-x-1/2 mt-5 px-2 w-screen max-w-md sm:px-0">
 								<div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
 									<div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-										<a href="#" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+										<nuxt-link :to="{ name : '' }" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 											<!-- Heroicon name: outline/support -->
 											<svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
@@ -111,9 +111,9 @@
 													Get all of your questions answered in our forums or contact support.
 												</p>
 											</div>
-										</a>
+										</nuxt-link>
 
-										<a href="#" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+										<nuxt-link :to="{ name : 'contact' }" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 											<!-- Heroicon name: outline/bookmark-alt -->
 											<svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
@@ -126,9 +126,9 @@
 													Learn how to maximize our platform to get the most out of it.
 												</p>
 											</div>
-										</a>
+										</nuxt-link>
 
-										<a href="#" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+										<nuxt-link :to="{ name : '' }" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 											<!-- Heroicon name: outline/calendar -->
 											<svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
@@ -141,7 +141,7 @@
 													See what meet-ups and other events we might be planning near you.
 												</p>
 											</div>
-										</a>
+										</nuxt-link>
 									</div>
 								</div>
 							</div>
@@ -149,9 +149,9 @@
 					</div>
 				</nav>
 				<div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-					<a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-base-red-darken-3">
+					<nuxt-link :to="{ name : '' }" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-base-red-darken-3">
 						Boutique
-					</a>
+					</nuxt-link>
 				</div>
 			</div>
 
@@ -176,39 +176,39 @@
 
 							<div class="mt-6">
 								<nav class="pt-2 pb-3 space-y-1">
-									<a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">News & Articles</a>
+									<nuxt-link :to="{ name : '' }" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">News & Articles</nuxt-link>
 
-									<a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Esport</a>
+									<nuxt-link :to="{ name : '' }" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Esport</nuxt-link>
 
-									<a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">WebTV</a>
+									<nuxt-link :to="{ name : '' }" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">WebTV</nuxt-link>
 
-									<a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Tournois</a>
+									<nuxt-link :to="{ name : '' }" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Tournois</nuxt-link>
 
-									<a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Partenaires</a>
+									<nuxt-link :to="{ name : '' }" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Partenaires</nuxt-link>
 
-									<a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Donations</a>
+									<nuxt-link :to="{ name : '' }" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Donations</nuxt-link>
 								</nav>
 							</div>
 						</div>
 						<div class="py-6 px-5">
 							<div class="grid grid-cols-2 gap-4">
 
-								<a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
+								<nuxt-link :to="{ name : '' }" class="text-base font-medium text-gray-900 hover:text-gray-700">
 									A propos
-								</a>
+								</nuxt-link>
 
-								<a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
+								<nuxt-link :to="{ name : 'contact' }" class="text-base font-medium text-gray-900 hover:text-gray-700">
 									Contact
-								</a>
+								</nuxt-link>
 
-								<a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
+								<nuxt-link :to="{ name : '' }" class="text-base font-medium text-gray-900 hover:text-gray-700">
 									Recrutement
-								</a>
+								</nuxt-link>
 							</div>
 							<div class="mt-6">
-								<a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-base-red-darken-3">
+								<nuxt-link :to="{ name : '' }" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-base-red-darken-3">
 									Boutique
-								</a>
+								</nuxt-link>
 							</div>
 						</div>
 					</div>
@@ -241,6 +241,12 @@ export default {
 			if (this.teamFlyoutMenu) {
 				this.teamFlyoutMenu = false
 			}
+		}
+	},
+
+	watch: {
+		'$route.name'() {
+			this.mobileMenu = false
 		}
 	},
 
