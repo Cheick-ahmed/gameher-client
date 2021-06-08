5ba4d90460739642f669709e6297733ac6cc6a8f
Sidibé Cheick Ahmed <chs.ahmed_pro02@outlook.com>
2021-06-08 15:06:31 +0200

    feat: add contact page

---
+++ pages/contact.vue
@@ -0,0 +1,258 @@
+<template>
+	<main class="overflow-hidden">
+		<!-- Header -->
+		<div class="bg-warm-gray-50">
+			<div class="py-20">
+				<div class="relative z-10 max-w-7xl mx-auto pl-4 pr-8 sm:px-6 lg:px-8">
+					<h1 class="text-4xl font-extrabold tracking-tight text-warm-gray-900 sm:text-5xl lg:text-6xl">Contactez-nous</h1>
+					<p class="mt-6 text-xl text-warm-gray-500 max-w-3xl">
+						Nous sommes ouverts au public du lundi au vendredi de 8h à 20h. N'hésitez pas à contacter notre équipe via ce formulaire pour toute question.
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<!-- Contact section -->
+		<section class="relative bg-white" aria-labelledby="contactHeading">
+			<div class="absolute w-full h-1/2 bg-warm-gray-50" aria-hidden="true"></div>
+			<!-- Decorative dot pattern -->
+			<div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				<svg class="absolute z-0 top-0 right-0 transform -translate-y-16 translate-x-1/2 sm:translate-x-1/4 md:-translate-y-24 lg:-translate-y-72" width="404" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true">
+					<defs>
+						<pattern id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+							<rect x="0" y="0" width="4" height="4" class="text-warm-gray-200" fill="currentColor" />
+						</pattern>
+					</defs>
+					<rect width="404" height="384" fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+				</svg>
+			</div>
+			<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				<div class="relative bg-white shadow-xl">
+					<h2 id="contactHeading" class="sr-only">Contactez-nous</h2>
+
+					<div class="grid grid-cols-1 lg:grid-cols-3">
+						<!-- Contact information -->
+						<div class="relative overflow-hidden py-10 px-6 bg-gradient-to-b from-gray-900 to-base-red-darken-3 sm:px-10 xl:p-12">
+							<!-- Decorative angle backgrounds -->
+							<div class="absolute inset-0 pointer-events-none sm:hidden" aria-hidden="true">
+								<svg class="absolute inset-0 w-full h-full" width="343" height="388" viewBox="0 0 343 388" fill="none" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg">
+									<path d="M-99 461.107L608.107-246l707.103 707.107-707.103 707.103L-99 461.107z" fill="url(#linear1)" fill-opacity=".1" />
+									<defs>
+										<linearGradient id="linear1" x1="254.553" y1="107.554" x2="961.66" y2="814.66" gradientUnits="userSpaceOnUse">
+											<stop stop-color="#fff"></stop>
+											<stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+										</linearGradient>
+									</defs>
+								</svg>
+							</div>
+							<div class="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none sm:block lg:hidden" aria-hidden="true">
+								<svg class="absolute inset-0 w-full h-full" width="359" height="339" viewBox="0 0 359 339" fill="none" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg">
+									<path d="M-161 382.107L546.107-325l707.103 707.107-707.103 707.103L-161 382.107z" fill="url(#linear2)" fill-opacity=".1" />
+									<defs>
+										<linearGradient id="linear2" x1="192.553" y1="28.553" x2="899.66" y2="735.66" gradientUnits="userSpaceOnUse">
+											<stop stop-color="#fff"></stop>
+											<stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+										</linearGradient>
+									</defs>
+								</svg>
+							</div>
+							<div class="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none lg:block" aria-hidden="true">
+								<svg class="absolute inset-0 w-full h-full" width="160" height="678" viewBox="0 0 160 678" fill="none" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg">
+									<path d="M-161 679.107L546.107-28l707.103 707.107-707.103 707.103L-161 679.107z" fill="url(#linear3)" fill-opacity=".1" />
+									<defs>
+										<linearGradient id="linear3" x1="192.553" y1="325.553" x2="899.66" y2="1032.66" gradientUnits="userSpaceOnUse">
+											<stop stop-color="#fff"></stop>
+											<stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+										</linearGradient>
+									</defs>
+								</svg>
+							</div>
+							<h3 class="text-lg font-medium text-white">Contact information</h3>
+							<p class="mt-6 text-base text-teal-50 max-w-3xl">Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.</p>
+							<dl class="mt-8 space-y-6">
+								<dt><span class="sr-only">Email</span></dt>
+								<dd class="flex text-base text-teal-50">
+									<svg class="flex-shrink-0 w-6 h-6 text-base-yellow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+									</svg>
+									<span class="ml-3">contact@gameher.fr</span>
+								</dd>
+
+								<dt><span class="sr-only">Partners</span></dt>
+								<dd class="flex text-base text-teal-50">
+									<svg class="flex-shrink-0 w-6 h-6 text-base-yellow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+									</svg>
+									<span class="ml-3">partenariat@gameher.fr</span>
+								</dd>
+
+								<dt><span class="sr-only">Phone number</span></dt>
+								<dd class="flex text-base text-teal-50">
+									<svg class="flex-shrink-0 w-6 h-6 text-base-yellow" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path></svg>
+									<span class="ml-3">esport@gameher.fr</span>
+								</dd>
+
+								<dt><span class="sr-only">Recrutement email</span></dt>
+								<dd class="flex text-base text-teal-50">
+									<svg class="flex-shrink-0 w-6 h-6 text-base-yellow" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
+									<span class="ml-3">recrutement@gameher.fr</span>
+								</dd>
+							</dl>
+							<ul class="mt-10 flex space-x-8" role="list">
+								<li>
+									<a class="text-base-yellow hover:text-teal-100" href="https://twitter.com/GameHer" target="_blank" rel="nofollow noopener noreferrer">
+										<span class="sr-only">Twitter</span>
+										<svg class="w-7 h-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+											<path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+										</svg>
+									</a>
+								</li>
+								<li>
+									<a class="text-base-yellow hover:text-teal-100" href="https://www.facebook.com/GameHer" target="_blank" rel="nofollow noopener noreferrer">
+										<span class="sr-only">Facebook</span>
+										<svg class="w-7 h-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+											<path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
+										</svg>
+									</a>
+								</li>
+								<li>
+									<a class="text-base-yellow hover:text-teal-100" href="#">
+										<span class="sr-only">Youtube</span>
+										<svg class="h-7 w-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path></svg>
+									</a>
+								</li>
+								<li>
+									<a class="text-base-yellow hover:text-teal-100" href="#">
+										<span class="sr-only">Twitch</span>
+										<svg class="h-7 w-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitch" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+											<path fill="currentColor" d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z" />
+										</svg>
+									</a>
+								</li>
+								<li>
+									<a class="text-base-yellow hover:text-teal-100" href="#">
+										<span class="sr-only">Discord</span>
+										<svg class="h-7 w-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="discord" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+											<path fill="currentColor" d="M297.216 243.2c0 15.616-11.52 28.416-26.112 28.416-14.336 0-26.112-12.8-26.112-28.416s11.52-28.416 26.112-28.416c14.592 0 26.112 12.8 26.112 28.416zm-119.552-28.416c-14.592 0-26.112 12.8-26.112 28.416s11.776 28.416 26.112 28.416c14.592 0 26.112-12.8 26.112-28.416.256-15.616-11.52-28.416-26.112-28.416zM448 52.736V512c-64.494-56.994-43.868-38.128-118.784-107.776l13.568 47.36H52.48C23.552 451.584 0 428.032 0 398.848V52.736C0 23.552 23.552 0 52.48 0h343.04C424.448 0 448 23.552 448 52.736zm-72.96 242.688c0-82.432-36.864-149.248-36.864-149.248-36.864-27.648-71.936-26.88-71.936-26.88l-3.584 4.096c43.52 13.312 63.744 32.512 63.744 32.512-60.811-33.329-132.244-33.335-191.232-7.424-9.472 4.352-15.104 7.424-15.104 7.424s21.248-20.224 67.328-33.536l-2.56-3.072s-35.072-.768-71.936 26.88c0 0-36.864 66.816-36.864 149.248 0 0 21.504 37.12 78.08 38.912 0 0 9.472-11.52 17.152-21.248-32.512-9.728-44.8-30.208-44.8-30.208 3.766 2.636 9.976 6.053 10.496 6.4 43.21 24.198 104.588 32.126 159.744 8.96 8.96-3.328 18.944-8.192 29.44-15.104 0 0-12.8 20.992-46.336 30.464 7.68 9.728 16.896 20.736 16.896 20.736 56.576-1.792 78.336-38.912 78.336-38.912z" />
+										</svg>
+									</a>
+								</li>
+								<li>
+									<a class="text-base-yellow hover:text-teal-100" href="#">
+										<span class="sr-only">Instagram</span>
+										<svg class="h-7 w-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+											<path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
+										</svg>
+									</a>
+								</li>
+							</ul>
+						</div>
+
+						<!-- Contact form -->
+						<div class="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
+							<h3 class="text-lg font-medium text-warm-gray-900">Send us a message</h3>
+							<form action="#" method="POST" class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+								<div>
+									<label for="first_name" class="block text-sm font-medium text-warm-gray-900">Votre prénom</label>
+									<div class="mt-1">
+										<input type="text" name="first_name" id="first_name" autocomplete="given-name" class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-base-yellow focus:border-base-yellow border-warm-gray-300 rounded-md">
+									</div>
+								</div>
+								<div>
+									<label for="last_name" class="block text-sm font-medium text-warm-gray-900">Votre nom</label>
+									<div class="mt-1">
+										<input type="text" name="last_name" id="last_name" autocomplete="family-name" class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-base-yellow focus:border-base-yellow border-warm-gray-300 rounded-md">
+									</div>
+								</div>
+								<div>
+									<label for="email" class="block text-sm font-medium text-warm-gray-900">Votre Email</label>
+									<div class="mt-1">
+										<input id="email" name="email" type="email" autocomplete="email" class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-base-yellow focus:border-base-yellow border-warm-gray-300 rounded-md">
+									</div>
+								</div>
+								<div>
+									<div class="flex justify-between">
+										<label for="phone" class="block text-sm font-medium text-warm-gray-900">Votre téléphone</label>
+										<span id="phone-optional" class="text-sm text-warm-gray-500">Facultatif</span>
+									</div>
+									<div class="mt-1">
+										<input type="text" name="phone" id="phone" autocomplete="tel" class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-base-yellow focus:border-base-yellow border-warm-gray-300 rounded-md" aria-describedby="phone-optional">
+									</div>
+								</div>
+								<div class="sm:col-span-2">
+									<div class="flex justify-between">
+										<label for="message" class="block text-sm font-medium text-warm-gray-900">La raison pour laquelle vous nous contactez</label>
+										<span id="message-max" class="text-sm text-warm-gray-500">Max. 500 characters</span>
+									</div>
+									<div class="mt-1">
+										<textarea id="message" name="message" rows="4" class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-base-yellow focus:border-base-yellow border border-warm-gray-300 rounded-md" aria-describedby="message-max"></textarea>
+									</div>
+								</div>
+								<div class="sm:col-span-2 sm:flex sm:justify-end">
+									<button type="submit" class="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-base-red-darken-3 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-base-yellow sm:w-auto">
+										Contactez-nous
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Contact grid -->
+		<section aria-labelledby="officesHeading">
+			<div class="max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+				<h2 id="officesHeading" class="text-3xl font-extrabold text-warm-gray-900">Nous trouver</h2>
+				<p class="mt-6 text-lg text-warm-gray-500 max-w-3xl">Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.</p>
+				<div class="mt-10 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-4">
+					<div>
+						<h3 class="text-lg font-medium text-warm-gray-900">Nantes</h3>
+						<p class="mt-2 text-base text-warm-gray-500">
+							<span class="block">1 place George Washington</span>
+
+							<span class="block">44300, Nantes</span>
+						</p>
+					</div>
+
+					<div>
+						<h3 class="text-lg font-medium text-warm-gray-900">New York</h3>
+						<p class="mt-2 text-base text-warm-gray-500">
+							<span class="block">886 Walter Streets</span>
+
+							<span class="block">New York, NY 12345</span>
+						</p>
+					</div>
+
+					<div>
+						<h3 class="text-lg font-medium text-warm-gray-900">Toronto</h3>
+						<p class="mt-2 text-base text-warm-gray-500">
+							<span class="block">7363 Cynthia Pass</span>
+
+							<span class="block">Toronto, ON N3Y 4H8</span>
+						</p>
+					</div>
+
+					<div>
+						<h3 class="text-lg font-medium text-warm-gray-900">London</h3>
+						<p class="mt-2 text-base text-warm-gray-500">
+							<span class="block">114 Cobble Lane</span>
+
+							<span class="block">London N1 2EF</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+
+		}
+	}
+}
+</script>
