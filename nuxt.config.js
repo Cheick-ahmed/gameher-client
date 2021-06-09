997ebb49598d3c55b5bfb9951a8c0908ec71c5c3
Sidibé Cheick Ahmed <chs.ahmed_pro02@outlook.com>
2021-06-09 17:09:15 +0200

    update site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
   head: {
-    title: 'client',
+    title: 'GameHer',
     htmlAttrs: {
       lang: 'en'
     },
